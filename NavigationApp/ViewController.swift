94becee0336fd4afeba73ace753ad25d65130211
DMYTRO <dmytro.neboha@proton.me>
2022-10-19 21:59:55 +0300

    Create a Navigation Interface Programmatically

--- NavigationApp/ViewController.swift
+++ NavigationApp/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  NavigationApp
-//
-//  Created by Dmytro Neboha on 19.10.2022.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // ссылка на сториборд, где размещен данный ViewController
+    let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Визуальное оформление Navigation Item можно определить и кодом тут:
+//        self.navigationItem.prompt = "Подсказка"
+//        self.navigationItem.title = "Заголовок сцены"
+//        self.navigationItem.backButtonTitle = "Назад"
 
     }
 
+    // перейти к зеленой сцене
+    @IBAction func toGreenScene(_ sender: UIButton) {
+        // получаем ссылку на следующий контроллер, в данном случае это зеленый
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "greenViewController")
+        // обращаемся к Navigation COntroller и вызываем метод перехода к новому контроллеру
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    // перейти к желтой сцене
+    @IBAction func toYellowScene(_ sender: UIButton) {
+        // получаем ссылку на следующий контроллер, в данном случае это зеленый
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "yellowViewController")
+        // обращаемся к Navigation COntroller и вызываем метод перехода к новому контроллеру
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
 
+    @IBAction func toRootScene(_ sender: UIButton) {
+        // обращаемся к Navigation COntroller и вызываем метод перехода к корневому контроллеру
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func toPreviousScene(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
 
